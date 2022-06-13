7bbe9ce2c43b0334185ee1cb851108819a44deb5
YunA486 <w2039@e-mirim.hs.kr>
2022-06-13 13:43:04 +0900

    accounts/forms LoginForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 from accounts.models import Profile
 
@@ -21,3 +22,10 @@ class RegisterForm(UserCreationForm):
             nickname=self.cleaned_data.get('nickname')  # self.cleaned_date['nickname']
         )
         return new_profile
+
+class LoginForm(models.ModelForm):
+    password = forms.CharField(label='패스워드', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password')
