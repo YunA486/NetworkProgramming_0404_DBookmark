539aba5733714de8088610903d303c84038dac9d
YunA486 <w2039@e-mirim.hs.kr>
2022-06-20 13:46:57 +0900

    FBV list_bookmark

--- bookmark/views.py
+++ bookmark/views.py
@@ -45,3 +45,15 @@ class BookmarkUpdateView(LoginRequiredMixin, UpdateView):
 class BookmarkDeleteView(LoginRequiredMixin, DeleteView):
     model = Bookmark
     success_url = reverse_lazy('bookmark:list')
+
+def list_bookmark(request):
+
+    # 로그인 사용자 확인하기
+    user = request.user
+    if user.is_authenticated:    # 로그인 되어있으면
+        profile = Profile.objects.get(user=user)
+        bookmark_list = Bookmark.objects.filter(profile=profile)    # 그 사용자의 북마크 가져오기
+    else:    # 로그인 안되어 있으면
+        bookmark_list = Bookmark.objects.none()     # 북마크 없는 것 가져오기
+
+    return render(request, 'bookmark/bookmark_list.html', {'bookmark_list' : bookmark_list})
